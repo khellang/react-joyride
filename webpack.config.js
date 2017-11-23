8f9057fb4a5285c007e7519e15811387481eac64
Nate Wildermuth <nate.wildermuth@gmail.com>
2017-11-23 03:59:06 -0600

    Added webpack integration and optimized builds to dist directory

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  entry: './src/scripts/index.jsx',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    library: "Joyride",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader?forceEnv=production',
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+};
